7d58e6a439571c45d90964211181a474a6fcf8f2
Javier <javiergaspar07@gmail.com>
2022-06-05 14:18:43 -0400

    settings.py - DEBUG variable turned to False.

--- geolocation/settings.py
+++ geolocation/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['habyehgeolocation.herokuapp.com', '127.0.0.1']
 
