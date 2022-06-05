8a14dcd3d234b35ce48e027b5b0f09350ecc55fa
Javier <javiergaspar07@gmail.com>
2022-06-05 11:42:30 -0400

    Providers URLs registered.

--- geolocation/urls.py
+++ geolocation/urls.py
@@ -2,8 +2,11 @@
 geolocation URL Configuration
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Providers
+    path('', include(('providers.urls.module_urls', 'providers'), namespace='providers')),
 ]
