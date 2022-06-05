c48889fc4c542486b1015d6aec38aaa45192d5cf
Javier <javiergaspar07@gmail.com>
2022-06-05 13:47:07 -0400

    Procfile created. Allowed hosts updated.

--- geolocation/settings.py
+++ geolocation/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://habyehgeolocation.herokuapp.com/', '127.0.0.1']
 
 # Application definition
 
